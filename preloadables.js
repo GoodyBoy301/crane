ff7edc17b2ff1091519e50d296d967d8cb944b88
goodness <goodness@revivar.app>
2023-02-13 20:21:43 +0100

    created preloadables, added images

--- preloadables.js
+++ preloadables.js
@@ -1,7 +1,51 @@
-const preloadables = {};
-preloadables.images = [];
-preloadables.videos = [];
-preloadables.textures = {};
-preloadables.exports = {};
+const preloadables = {}
+preloadables.images = []
+preloadables.home = {
+  heroImage1:
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147518/Cranne/that_s_a_satisfying_perfect_circle_twlagx.jpg",
+  heroImage2: "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147530/Cranne/Next___Kamui_f5fpft.png",
+  slideImage1: "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147523/Cranne/Tokyo___Eiji_Fukui_odbpd2.png",
+  slideImage2: "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147530/Cranne/Next___Kamui_f5fpft.png",
+  slideImage3:
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147518/Cranne/that_s_a_satisfying_perfect_circle_twlagx.jpg",
+  slideImage4: "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147522/Cranne/Elite_London_churtx.png",
+  slideImage5: "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147557/Cranne/URBAN_PARADISE_ymhoxp.png",
+  slideImage6:
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147510/Cranne/caffe942-0fd8-4629-b47d-517d383af344_feihgx.jpg",
+  aboutImage1:
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147616/Cranne/caffe942-0fd8-4629-b47d-517d383af344_2_uggcqe.png",
+  carouselImage1:"",
+  carouselImage1:"",
+  carouselImage1:"",
+}
+preloadables.models = {
+  ayoola: [
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147524/Cranne/tumblr_fdd4a1b8fa5b557a7a82578d5a569ffd_d5092236_1280_kgncel.jpg",
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147522/Cranne/tumblr_7de3e305c57cc250f4224a843cf0615b_7abe99da_1280_fvdgu6.jpg",
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147525/Cranne/tumblr_oxa8adSKOx1vftsnoo2_1280_zzxn1b.jpg",
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147614/Cranne/Rectangle_8_a2189p.png",
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147620/Cranne/Rectangle_9_tk6yym.png",
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147627/Cranne/Rectangle_10_ejwdlg.png",
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147627/Cranne/Rectangle_11_wmupxc.png",
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147634/Cranne/Rectangle_12_dvrxxo.png",
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147616/Cranne/Rectangle_13_th7wjt.png",
+  ],
+  olacodes: ["https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147523/Cranne/Tokyo___Eiji_Fukui_odbpd2.png"],
+  davidella: [
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147518/Cranne/that_s_a_satisfying_perfect_circle_twlagx.jpg",
+  ],
+  stephine: [
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147515/Cranne/79ee9ee7-75c5-45b4-85ae-21ac398408f5_il3rpm.jpg",
+  ],
+  gwen: ["https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147511/Cranne/Learn_to_Love_My_Hair_lujgfz.jpg"],
+  david: ["https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147522/Cranne/Elite_London_churtx.png"],
+  chisom: [
+    "https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147509/Cranne/66404E00-7067-4B2C-871F-7D42F6FD0701_j39980.jpg",
+  ],
+  juane: ["https://res.cloudinary.com/ddep1mxpn/image/upload/v1675147530/Cranne/Next___Kamui_f5fpft.png"],
+}
+preloadables.videos = []
+preloadables.textures = {}
+preloadables.exports = {}
 
-module.exports = preloadables;
+module.exports = preloadables
